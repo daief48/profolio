dd21713eeac4840347ba51a9ac0ccb8f6293e1e4
daief48 <daief20103048@gmail.com>
2023-08-09 03:41:59 +0600

    final add

---
+++ src/content_option.js
@@ -0,0 +1,226 @@
+const logotext = "Daief";
+const meta = {
+    title: "Jubair Islam Daief",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Jubair Islam Daief",
+    animated: {
+        first: "I love coding",
+        second: "I code Forntend and Backend",
+        third: "I develop Mern Stack App",
+    },
+    description: "Passionate MERN stack developer with skills in HTML, CSS, Bootstrap, and JavaScript. Creating engaging web applications and sharing knowledge through Campus Lite's programming YouTube channel. Eager to collaborate on exciting projects and make a positive impact in the tech world. Let's connect and build something exceptional!",
+    your_img_url: "/img2.png",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Passionate MERN stack developer with skills in HTML, CSS, Bootstrap, and JavaScript. Creating engaging web applications and sharing knowledge through Campus Lite's programming YouTube channel. Eager to collaborate on exciting projects and make a positive impact in the tech world. Let's connect and build something exceptional!",
+};
+const worktimeline = [
+    {
+        jobtitle: "Programming toutor in Youtube",
+        where: "Campus Lite",
+        date: "2020",
+    },
+    {
+        jobtitle: "React Toutor in Wtsolver",
+        where: "Dhaka, Bangladesh",
+        date: "2021",
+    }
+];
+
+const skills = [{
+        name: "React",
+        value: 90,
+    },
+    {
+        name: "Mern Stack",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "HTML",
+        value: 60,
+    },
+    {
+        name: "CSS",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "MERN Stack",
+        description: "I am a skilled MERN stack developer with expertise in building web applications using MongoDB, Express.js, React, and Node.js. I have successfully completed three major projects based on the MERN stack, including an eCommerce platform, a social media application, and a messenger app.",
+    },
+    {
+        title: "Front-End Skills",
+        description: "I am proficient in HTML, CSS, and JavaScript, and I have a strong understanding of web design principles. Additionally, I am well-versed in Bootstrap, allowing me to create responsive and visually appealing user interfaces.",
+    },
+    {
+        title: "React",
+        description: "I have mastered React, a popular JavaScript library for building user interfaces. With my in-depth knowledge of React, I can efficiently create dynamic and interactive components for web applications.",
+    },
+];
+
+const dataportfolio = [
+    {
+        img: "project/ecommerce_mern.png",
+        description: "Ecommerce Website - Mern Stack",
+        link: "https://ecommerce-mern-app-frontend1.onrender.com/",
+    },
+    {
+        img: "project/netfilx.png",
+        description: "Netfilx Website - React Js",
+        link: "https://netfilx-react-app.onrender.com/",
+    },
+    {
+        img: "project/accordion.png",
+        description: "Accorion App - React Js.",
+        link: "https://accordion-react-app.onrender.com/",
+    },
+    {
+        img: "project/random image.png",
+        description: "Random Image - React Js",
+        link: "https://random-images-react-app.onrender.com/",
+    },
+    {
+        img: "project/Food Html Css.png",
+        description: "Food  - Html Css",
+        link: "https://daief48.github.io/food_forntend_html_css/",
+    },
+    {
+        img: "project/todo_material.png",
+        description: "Todo material - React JS.",
+        link: "https://todo-react-app-with-material-ui.onrender.com",
+    },
+
+    {
+        img: "project/digital clock.png",
+        description: "Digita Clock - React Js",
+        link: "https://digital-clock-react-app.onrender.com/",
+    },
+    {
+        img: "project/slot machine.png",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://slot-game-react-app.onrender.com/",
+    },
+    {
+        img: "project/imge_upload.png",
+        description: "Image Upload - Mern Stack.",
+        link: "https://imageupload-mern-forntend.onrender.com/",
+    },
+    {
+        img: "project/keep_app.png",
+        description: "Keep App - React Js",
+        link: "https://keep-react-app.onrender.com/",
+    },
+    {
+        img: "project/from react.png",
+        description: "From - React Js",
+        link: "https://from-react-app.onrender.com/",
+    },
+    {
+        img: "project/todo.png",
+        description: "Todo App - React App",
+        link: "https://todo-react-app-0qpu.onrender.com/",
+    },
+    
+    ////////////////////////////////////////////////////////
+    {
+        img: "project/greeting.png",
+        description: "Geeting - React Js",
+        link: "https://greeating-react-app.onrender.com/",
+    },
+    {
+        img: "project/navbar routing.png",
+        description: "Navbar Routing - React Js",
+        link: "https://navbar-react-app-routing.onrender.com/",
+    },
+    {
+        img: "project/inc dec.png",
+        description: "Increment Decrement App - React Js.",
+        link: "https://daief48-increment-decrement-react-app.onrender.com/",
+    },
+    {
+        img: "project/coffee html css.png",
+        description: "Coffee - Html Css",
+        link: "https://daief48.github.io/Coffee_Shop_html_css/",
+    },
+    {
+        img: "project/blog.png",
+        description: "Blog Website - Mern Stack",
+        link: "https://blog-mern-nic1.onrender.com",
+    },
+    {
+        img: "project/flex.png",
+        description: "Flex Box - Html Css",
+        link: "https://daief48.github.io/flexbox_practice/",
+    },
+
+    {
+        img: "project/calculator.png",
+        description: "Digita Clock - React Js",
+        link: "https://daief48.github.io/Simple-JavaScript-Calculator-master/",
+    },
+    {
+        img: "project/slot machine.png",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://slot-game-react-app.onrender.com/",
+    },
+    {
+        img: "project/imge_upload.png",
+        description: "Image Upload - Mern Stack.",
+        link: "https://imageupload-mern-forntend.onrender.com/",
+    },
+    {
+        img: "project/keep_app.png",
+        description: "Keep App - React Js",
+        link: "https://keep-react-app.onrender.com/",
+    },
+    {
+        img: "project/from react.png",
+        description: "From - React Js",
+        link: "https://from-react-app.onrender.com/",
+    },
+    {
+        img: "project/todo.png",
+        description: "Todo App - React App",
+        link: "https://todo-react-app-0qpu.onrender.com/",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "daiefsikder425@gmail.com",
+    YOUR_FONE: "+88 01924529986",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/daief48",
+    facebook: "https://www.facebook.com/da1efsikder/",
+    linkedin: "https://www.linkedin.com/in/daief-sikder-059b57207/",
+    twitter: "https://twitter.com/Daief86362",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
